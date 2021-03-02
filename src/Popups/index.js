988dd716c17369fa3da6560b09451142c308a4d9
stephen.lally <stephen.lally.smbc@gmail.com>
2021-03-02 11:33:52 +0000

    feat(Style): changed to DS styling

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,24 +1,40 @@
 const wardPopup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-list" aria-hidden="true"></i><p class="title">Ward</p>
-    <p></p>
-    <p class="info">Name: ${feature.properties.ward_name}</p>
-    </div>`
+  const content = `<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="fa fa-list smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Ward</span>
+    </div>
+      <div class="smbc-map__item__body">
+      <p>Name: ${feature.properties.ward_name}</p>
+    </div>
+  </div>`
   layer.bindPopup(content)
 }
 
 const polling_districtsPopup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-list" aria-hidden="true"></i><p class="title">Polling District</p>
-    <p></p>
-    <p class="info">Name: ${feature.properties.polling_name}</p>
-    </div>`
+  const content = `<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="fa fa-list smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Polling District</span>
+    </div>
+      <div class="smbc-map__item__body">
+        <p>Name: ${feature.properties.polling_name}</p>
+    </div>
+  </div>`
+
   layer.bindPopup(content)
 }
 
 const polling_stationsPopup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-list" aria-hidden="true"></i><p class="title">Polling Station</p>
-  <p></p>
-  <p class="info"> Polling Station No: ${feature.properties.station_no}</p>
-  <p class="info">Address: ${feature.properties.address}</p>
+  const content = `<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="fa fa-list smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Polling Station</span>
+    </div>
+      <div class="smbc-map__item__body">
+        <p> Polling Station No: ${feature.properties.station_no}</p>
+        <p>Address: ${feature.properties.address}</p>
+    </div>
   </div>`
   layer.bindPopup(content)
 }
